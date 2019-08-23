edf490179558bdb5e850f2206a0e0600fba3f0ec
dongbo <895188445@qq.com>
2019-08-23 21:45:48 +0800

    update

---
+++ readme.md
@@ -0,0 +1,18 @@
+# ceisum 使用demo、方法或components总结
+
+使用cesium+react+ts编写
+
+## 样例类容
+- 调整3dtiles高度
+- 地球自旋转
+- 加载3dtiles
+- 画河流
+
+## 组件
+- domTag （3d物体的dom标签）
+- domAnimationPoint (3d位置点或物体的 dom点扩散光环)
+
+
+## 交流
+
+qq群 334522247
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -13,3 +13,32 @@ export function PickLowestPostion(viewer: Cesium.Viewer, center: Cesium.Cartesia
     }
 
 }
+
+
+export function rotationTo(from: Cesium.Cartesian3, to: Cesium.Cartesian3, out: Cesium.Quaternion = new Cesium.Quaternion()): Cesium.Quaternion {
+    let tmpVec3 = new Cesium.Cartesian3();
+    let xUnitVec3 = Cesium.Cartesian3.UNIT_X;
+    let yUnitVec3 = Cesium.Cartesian3.UNIT_Y;
+
+    let dot = Cesium.Cartesian3.dot(from, to);
+    if (dot < -0.999999) {
+        Cesium.Cartesian3.cross(tmpVec3, xUnitVec3, from);
+        if (Cesium.Cartesian3.magnitude(tmpVec3) < 0.000001) Cesium.Cartesian3.cross(tmpVec3, yUnitVec3, from);
+        Cesium.Cartesian3.normalize(tmpVec3, tmpVec3);
+        Cesium.Quaternion.fromAxisAngle(tmpVec3, Math.PI, out);
+        return out;
+    } else if (dot > 0.999999) {
+        out.x = 0;
+        out.y = 0;
+        out.z = 0;
+        out.w = 1;
+        return out;
+    } else {
+        Cesium.Cartesian3.cross(tmpVec3, from, to);
+        out.x = tmpVec3.x;
+        out.y = tmpVec3.y;
+        out.z = tmpVec3.z;
+        out.w = 1 + dot;
+        return Cesium.Quaternion.normalize(out, out);
+    }
+}
